7cbe8b876ac190d938fc626dbb569cf8b5b41364
Dominic Humber <dominichumber@outlook.com>
2025-01-23 23:28:47 -0500

    Rename to typings

---
+++ util/typings.py
@@ -0,0 +1,39 @@
+from typing import Any, List, Optional, Generic, TypeVar, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+# Define a generic type variable
+T = TypeVar("T")
+
+
+class ConversationRecord(BaseModel):
+    id: str = Field(alias="_id")
+    user_id: int
+    guild_id: int
+    channel_id: int
+    messages: List[Any] = Field(default_factory=list)
+    last_updated: Optional[float] = Field(default_factory=lambda: datetime.now().timestamp())
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class Result(Generic[T]):
+    def __init__(self, success: bool, data: Optional[T] = None, error: Optional[str] = None):
+        self.success = success
+        self.data = data  # The type of data is now generic
+        self.error = error  # Error message (string)
+
+    @staticmethod
+    def success(data: T = None) -> "Result[T]":
+        return Result(True, data=data)
+
+    @staticmethod
+    def failure(error: str) -> "Result[T]":
+        return Result(False, error=error)
+
+    def __repr__(self):
+        if self.success:
+            return f"Result(success=True, data={self.data})"
+        else:
+            return f"Result(success=False, error='{self.error}')"
