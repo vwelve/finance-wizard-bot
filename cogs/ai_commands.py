48e3618c88c6dbbd090123799ad9d886e2594930
Dominic Humber <dominichumber@outlook.com>
2025-01-26 20:02:18 -0500

    Validate user's permissions and log

--- cogs/ai_commands.py
+++ cogs/ai_commands.py
@@ -4,10 +4,13 @@ from datetime import datetime
 import discord
 from discord import app_commands
 from discord.ext import commands
+
+from config import SECRET_CHANNEL_ID, SPECIAL_ROLE_ID
 from services.ai_service import AIService
 from services.database import DatabaseService
-from util import db
+from util import db, get_logger
 
+logger = get_logger(__name__)
 
 class AICommands(commands.Cog):
     def __init__(self, bot: commands.Bot, db_service: DatabaseService, ai: AIService):
@@ -18,10 +21,20 @@ class AICommands(commands.Cog):
     # Command to talk to the AI in the private channel
     @app_commands.command(name="ask", description="Ask the AI a question.")
     async def ask_ai(self, interaction: discord.Interaction, *, content: str):
-        await interaction.response.defer(ephemeral=True, thinking=True)
+        await interaction.response.defer(thinking=True)
 
         guild = interaction.guild
         user = interaction.user
+
+        logger.info(f"Checking {SECRET_CHANNEL_ID} is eq to {interaction.channel_id}")
+        if SECRET_CHANNEL_ID != interaction.channel_id:
+            await interaction.followup.send(f"You can only use this command in <#{SECRET_CHANNEL_ID}>")
+            return
+
+        logger.info(f"Checking if user {user.id} has role: {SPECIAL_ROLE_ID}")
+        if SPECIAL_ROLE_ID not in [role.id for role in user.roles]:
+            await interaction.followup.send(f"You don't have the required role to use this command.")
+            return
 
         result = self.db.get_conversation_record(guild.id, user.id)
 
