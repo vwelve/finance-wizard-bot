1ed481c1d4b5d066ff4fc60644adbd5b682f1ded
Dominic Humber <dominichumber@outlook.com>
2025-01-26 19:46:59 -0500

    Update ai_commands.py

--- cogs/ai_commands.py
+++ cogs/ai_commands.py
@@ -1,47 +1,42 @@
 # cogs/ai_commands.py
+from datetime import datetime
 
 import discord
 from discord import app_commands
 from discord.ext import commands
-
-from services import ConversationService
 from services.ai_service import AIService
 from services.database import DatabaseService
 from util import db
 
 
 class AICommands(commands.Cog):
-    def __init__(self, bot: commands.Bot, ai: AIService, conversation_service: ConversationService):
+    def __init__(self, bot: commands.Bot, db_service: DatabaseService, ai: AIService):
         self.bot = bot
         self.ai = ai
-        self.conversation_service = conversation_service
-
-    # Command to create a private AI channel
-    @app_commands.command(name="open", description="Open a private AI chat session.")
-    async def open_ai_chat(self, interaction: discord.Interaction):
-        await interaction.response.defer(ephemeral=True, thinking=True)
-        guild = interaction.guild
-        user = interaction.user
-
-        result = await self.conversation_service.get_user_conversation(guild, user)
-
-        if result.success:
-            await interaction.followup.send(f"You can send messages in <#{result.data.channel_id}>")
-            return
-
-        result = await self.conversation_service.create_conversation_channel(guild, user)
-        text_channel = result.data
-
-        if result.success:
-            # Create a private AI channel
-            await interaction.followup.send(f"Opening AI chat session in <#{text_channel.id}>")
-        else:
-            await interaction.followup.send(result.error)
+        self.db = db_service
 
     # Command to talk to the AI in the private channel
     @app_commands.command(name="ask", description="Ask the AI a question.")
-    async def ask_ai(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Asking AI a question...")
+    async def ask_ai(self, interaction: discord.Interaction, *, content: str):
+        await interaction.response.defer(ephemeral=True, thinking=True)
+
+        guild = interaction.guild
+        user = interaction.user
+
+        result = self.db.get_conversation_record(guild.id, user.id)
+
+        if not result.success:
+            await interaction.followup.send(result.error)
+
+        conversation = result.data
+        result = await self.ai.send_message(conversation, content)
+
+        if result.success:
+            data = result.data
+            chat_response = data[0]
+            await interaction.followup.send(chat_response.content)
+        else:
+            await interaction.followup.send(result.error)
 
     async def cog_load(self):
         pass
@@ -50,6 +45,5 @@ class AICommands(commands.Cog):
 async def setup(bot: commands.Bot):
     db_service = DatabaseService(db)
     ai_service = AIService(db_service)
-    conversation_service = ConversationService(db_service)
 
-    await bot.add_cog(AICommands(bot, ai_service, conversation_service))
+    await bot.add_cog(AICommands(bot, db_service, ai_service))
